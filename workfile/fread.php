6db7527d164c5ce40f1b71ca9394557bb2b188c1
Enyi <enyilio@liuenyide-MacBook-Pro.local>
2017-11-30 18:30:20 +0800

    file read and write

--- workfile/fread.php
+++ workfile/fread.php
@@ -2,8 +2,9 @@
 
 $file = "example.txt";
 
-if ($handle = fopen($file, 'r')) {
-	
+//fopen開啟example.txt文件,限定只能閱讀
+if ($handle = fopen($file, 'r')) { //打開文件$file "r"只能讀取
+	//filesize($file)表示顯示$file所有內容,也可以用字元來取代，例如：echo $content = fread($handle, 2);
 	echo $content = fread($handle, filesize($file));
 
 	fclose($handle);
--- workfile/fwrite.php
+++ workfile/fwrite.php
@@ -1,9 +1,9 @@
 <?php 
 
 $file = "example.txt";
-
+//打開$file檔案，"w"表示該檔案可寫入
 if ($handle = fopen($file, 'w')) {
-	
+	//fwrite表示寫入內容"I love Joomla!"
 	fwrite($handle, 'I love joomla!');
 	fclose($handle);
 }else{
